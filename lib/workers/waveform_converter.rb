a61ce3dba6acd3b4b7f7c430a638ac02d86e6b14
hoangphanea <hoang.phan@eastagile.com>
2016-02-22 13:03:10 +0700

    [r] Remove previous file

--- lib/workers/waveform_converter.rb
+++ lib/workers/waveform_converter.rb
@@ -6,6 +6,7 @@ class WaveformConverter
     tempname = "public/audios/#{id}.wav"
     path = "public/images/#{id}.png"
     system %Q{ffmpeg -y -i "public/audios/#{song.file_name}" -f wav "#{tempname}" > /dev/null 2>&1}
+    FileUtils.rm path
     Waveform.generate(tempname, path)
     FileUtils.rm tempname
     song.update(waveform_file: "#{id}.png")
