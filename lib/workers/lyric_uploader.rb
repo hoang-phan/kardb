484eef3ea6678756b715217486c6940e3da4d722
Hoang Phan <hoangphanbk10@gmail.com>
2016-02-27 08:22:43 -0800

    [f] Reformat lyric files

--- lib/workers/lyric_uploader.rb
+++ lib/workers/lyric_uploader.rb
@@ -2,23 +2,37 @@ class LyricUploader
   include Sidekiq::Worker
 
   def perform(id)
-    file_name = "#{id}.lyr"
+    lines_file_name = "ll_#{id}.csv"
+    words_file_name = "lw_#{id}.csv"
 
     song = Song.find(id)
-    File.open(file_name, 'w') do |f|
-      song.lines.each do |line|
-        f.write '-' 
-        content = line.words.pluck(:content, :note, :processed_at, :duration).map do |words|
-          words.map(&:to_s).join(',')
-        end.join("\n")
-        f.write content + "\n"
+    lines = song.lines.includes(:words)
+
+    CSV.open(lines_file_name, 'w') do |csv|
+      csv << %w(id position)
+      lines.each do |line|
+        csv << [line.id, line.position]
       end
     end
 
-    $client.put_file("/#{file_name}", open(file_name))
-    response = $session.do_get "/shares/auto/#{$client.format_path('/' + file_name)}", {"short_url"=>false}
+    CSV.open(words_file_name, 'w') do |csv|
+      csv << %w(id content processed_at duration note line_id)
+      lines.each do |line|
+        line.words.each do |word|
+          csv << [word.id, word.content, word.processed_at, word.duration, word.note, line.id]
+        end
+      end
+    end
+
+    $client.put_file("/#{lines_file_name}", open(lines_file_name))
+    response = $session.do_get "/shares/auto/#{$client.format_path('/' + lines_file_name)}", {"short_url"=>false}
     result = Dropbox::parse_response(response)
     song.update(lyric_link: result['url'].gsub('?dl=0', '?dl=1'))
+
+    $client.put_file("/#{words_file_name}", open(words_file_name))
+    response = $session.do_get "/shares/auto/#{$client.format_path('/' + words_file_name)}", {"short_url"=>false}
+    result = Dropbox::parse_response(response)
+    song.update(words_link: result['url'].gsub('?dl=0', '?dl=1'))
   rescue Exception => e
     p e
   end
